9ce4c9623377ab35360b3e13485a3a6d8bbce41b
santhosh-reddy-6404 <98030805+santhosh-reddy-6404@users.noreply.github.com>
2022-01-19 18:48:05 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+//Service Worker...
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('sw-cache').then(function(cache) {
+      return cache.add('index.html');
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
