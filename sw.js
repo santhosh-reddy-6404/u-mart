309d3335e3f01660d2706bf75612abfe9fd7527d
santhosh-reddy-6404 <98030805+santhosh-reddy-6404@users.noreply.github.com>
2022-01-20 18:54:39 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('sw-cache').then(function(cache) {
-      return cache.add('index.html');
+      return cache.add('README.md');
     })
   );
 });
